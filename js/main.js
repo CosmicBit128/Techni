28b108ec5c1aea8e18f7b217cbd4185c61985c91
CosmicBit128 <cosmicbit128@proton.me>
2026-04-29 12:40:39 +0200

    Test Events

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { send_notif } from "./utils/globals.js";
-import { initFromCover, swapCover } from "./background.js";
+// import { initFromCover, swapCover } from "./background.js";
 
 // send_notif("Błąd!", "Piosenka x nie działa! y, napraw ją.")
 
@@ -7,8 +7,48 @@ const activeBgRef = { current: null };
 
 window.addEventListener("load", () => {
     const cover = document.getElementById("song-cover");
+    const titles = document.getElementById("titles");
+    const slider = document.getElementById("seek");
 
-    const colorThief = new colorThief();
+    
+
+    const output = document.getElementById("debug");
+
+    function showEvent(text) {
+        output.textContent = text;
+    }
+
+    // Keyboard input (for testing arrows / buttons)
+    window.addEventListener("keydown", (event) => {
+        showEvent(`Key Down: ${event.key}`);
+    });
+
+    // Mouse click
+    window.addEventListener("click", (event) => {
+        showEvent(`Mouse Click: ${event.button}`);
+    });
+
+    titles.addEventListener("click", () => {
+        for (let i = 0; i < titles.children.length; i++) {
+            let title = titles.children[i];
+            if (title.dataset.text === "Techniplayer") {
+                title.dataset.text = "Techn!player";
+                title.textContent = "Techn!player";
+                // title.style.margin = '0 6px';
+            } else {
+                title.dataset.text = "Techniplayer";
+                title.textContent = "Techniplayer";
+                // title.style.margin = '0 5px';
+            }
+        }
+    });
+
+    slider.addEventListener("change", () => {
+        let percent = slider.value / slider.max * 100.0;
+        slider.style.background = `linear-gradient( 90deg, rgba(255, 255, 255, 0.9) ${percent-0.1}%, rgba(255, 255, 255, 0.4) ${percent+0.1}%)`;
+    });
+
+    const colorThief = new ColorThief();
     const initArgs = {
         cover,
         titleScope,
